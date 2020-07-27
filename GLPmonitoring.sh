4dd551a29bbdfedac0a43f2ce5faac95e08cc01e
Jeremy Lyon <jeremy@deskpc.jlyon.local>
2020-07-27 12:29:04 -0500

    Add file

---
+++ GLPmonitoring.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+
+#Check if Sudo
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit
+fi
+
+#Update/Upgrade
+apt update
+apt upgrade
+
+#Install Dependancies
+apt install -y unzip build-essential libsnmp-dev p7zip-full docker.io make golang apt-transport-https software-properties-common wget
+
+
+
+#Add Repos
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list 
+apt-get update
+
+#Install Grafana
+apt install grafana-enterprise
+systemctl daemon-reload
+systemctl start grafana-server
+systemctl enable grafana-server.service
+
+
+#Install Prometheus
+apt install prometheus
+
+#Install Loki
+cd /tmp
+sudo git clone https://github.com/grafana/loki $GOPATH/src/github.com/grafana/loki
+cd $GOPATH/src/github.com/grafana/loki
+make loki
+cd cmd/loki
+cp loki /usr/local/bin/loki
+cp loki-local-config.yaml /usr/local/bin/config-loki.yml
+nano /etc/systemd/system/loki.service #Add systemd service definition
+sudo systemctl start loki
+sudo systemctl enable loki
